c400ad8b274ad96dfcb403d481d236f56065d549
Memphis Marshall <memphism2003@gmail.com>
2024-04-10 14:57:15 +0800

    Update image to support testing

--- ctfcli/core/image.py
+++ ctfcli/core/image.py
@@ -1,9 +1,12 @@
+import click
 import json
+import socket
 import subprocess
 import tempfile
 from os import PathLike
 from pathlib import Path
-from typing import Optional, Union
+from timeit import default_timer
+from typing import Dict, List, Optional, Union
 
 
 class Image:
@@ -22,6 +25,38 @@ class Image:
         if build_path:
             self.build_path = Path(build_path)
             self.built = False
+        
+        # Should be None when no container is running and str when one is
+        # str with no container is an unexpected state
+        self.container = None
+        self._ip = None
+
+    @property
+    def ip(self) -> Optional[str]:
+        if not self.running:
+            return
+        if not self._ip:
+            result = subprocess.run(["docker", "inspect", self.container, "--format={{.NetworkSettings.IPAddress}}"], capture_output=True, text=True, check=True)
+            self._ip = result.stdout.strip()
+        return self._ip
+
+    @property
+    def running(self) -> bool:
+        if not self.container:
+            return False
+        result = subprocess.run(["docker", "inspect", self.container, "--format={{json .State.Running}}"], capture_output=True, text=True, check=True)
+        output = result.stdout.strip()
+        if output == "true":
+            return True
+        else:
+            # If we reach here, stop has not been called (self.container != None with no container running)
+            # Warn the user
+            click.secho(
+                f"Container from {self.name} exited unexpectedly.",
+                fg="yellow",
+            )
+            self.container = None
+            return False
 
     def build(self) -> Optional[str]:
         docker_build = subprocess.call(["docker", "build", "-t", self.name, "."], cwd=self.build_path.absolute())
@@ -30,6 +65,31 @@ class Image:
 
         self.built = True
         return self.name
+    
+    def run(self, environment: Dict[str, str]) -> Optional[str]:
+        if not self.built:
+            self.build()
+
+        command = ["docker", "run", "--rm", "-d"]
+        for variable in environment:
+            command.append("-e")
+            command.append(variable + "=" + environment[variable])
+        command.append(self.name)
+
+        result = subprocess.run(command, capture_output=True, text=True, check=True)
+        # Will return a container ID
+        self.container = result.stdout.strip()
+        return self.container
+    
+    def stop(self) -> Optional[str]:
+        if not self.running:
+            return
+        subprocess.check_call(["docker", "stop", self.container], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        return_value = self.container
+        # This means the container down state is expected
+        self.container = None
+        return return_value
+
 
     def pull(self) -> Optional[str]:
         docker_pull = subprocess.call(["docker", "pull", self.name])
@@ -61,8 +121,8 @@ class Image:
             return
 
         return image_tar.name
-
-    def get_exposed_port(self) -> Optional[str]:
+    
+    def _get_exposed_port_strings(self) -> Optional[List[str]]:
         if not self.built:
             self.build()
 
@@ -76,7 +136,45 @@ class Image:
         ports_data = json.loads(docker_output)
         if ports_data:
             ports = list(ports_data.keys())
+            return ports
+    
+    def ports_by_protocol(self, protocol: str = "tcp") -> Optional[List[int]]:
+        raw_ports = self._get_exposed_port_strings()
+        if not raw_ports:
+            return
+        ports = []
+        for port in raw_ports:
+            num, proto = port.split("/")
+            if proto == protocol:
+                ports.append(int(num))
+        return ports
+    
+    def wait_for_exposed_ports(self, timeout=30) -> bool:
+        if not self.running:
+            # We could run, but the user might have environment variables they want
+            return False
+        ports = self.ports_by_protocol("tcp")
+        start_time = default_timer()
 
-            if ports:
-                # Split '2323/tcp'
-                return ports[0].split("/")[0]
+        for port in ports:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(1)
+
+            while default_timer() - start_time > timeout:
+                if s.connect_ex((self.ip, port)):
+                    # Break out of the loop for this port,
+                    # indicating success
+                    break
+            else:
+                # If we exit the loop due to a timeout
+                click.secho(
+                    f"Timeout reached waiting for {self.name} to bring up exposed ports.",
+                    fg="yellow",
+                )
+                # Break out of the overarching loop
+                break
+        else:
+            # This runs only if we successfully connect to each port
+            return True
+        # And if we don't:
+        return False
